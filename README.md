4d11c5362db2097458225dcc4aef8f8e14dae9bc
benpote <ben.pote@gmail.com>
2014-01-10 11:51:41 -0500

    Initial commit: wrote README.md, .gitignore, working pseudonome.py

---
+++ README.md
@@ -0,0 +1,12 @@
+#Generating a pseudo-genome with Python
+
+A quick script which outputs a .fasta file with a given sequence length and gc content.
+
+___
+
+##Script usage:
+* pass the pseudonome function the following:
+	* Sequence name
+	* File name
+	* Length
+	* GC Content
---
+++ pseudogenome.py
@@ -0,0 +1,32 @@
+import random
+
+#get one random base pair (from actg)
+def randactg(gc):
+	x = random.random()
+	if x > gc:
+		y = random.choice('AT')
+	else:
+		y = random.choice('GC')
+	return y
+
+#get a random sequence length len
+def createseq(length,gc):
+	seq = ''
+	for i in range(length):
+		seq += randactg(gc)
+	return seq
+
+#write to a .fasta file with a given seqname, filename, and sequence
+def wrfasta(seqname,filename,seq):
+	file writefile = open (filename, 'w')
+	writefile.write('>' + seqname "\n" + seq)
+	writefile.close()
+	return None
+
+#tie it all together: pass a seqname, filename, sequence length, and gc content and it will write you a file
+def pseudonome(seqname,filename,length,gccont):
+	seq = createseq(length,gccont)
+	wrfasta(seqname,filename,seq)
+	return None
+	
+	
